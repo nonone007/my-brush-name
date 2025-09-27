a8f9bdd82402be131f70890f9d0fd1af6acffc44
nonone007 <bilelahmed0319@gmail.com>
2025-09-27 23:53:43 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',   // 👈 important for custom domain (like تجربة-الاسم.fazavip.tn)
+})
