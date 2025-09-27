6fd9928166805ac9e6a2fddc9033a67ca1e08f0d
nonone007 <bilelahmed0319@gmail.com>
2025-09-27 23:58:59 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',   // 👈 important for custom domain (like تجربة-الاسم.fazavip.tn)
-})
